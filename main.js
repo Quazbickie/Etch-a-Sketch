378de77753e44c102153cbfa820297e906a5b0ca
Ryan <ryankane180@gmail.com>
2023-10-30 20:35:11 +0000

    reset button

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-
+let resetBtn = document.querySelector('.reset');
 
 function createGrid(gridSize) {
     let grid = document.querySelector('.grid');
@@ -8,6 +7,7 @@ function createGrid(gridSize) {
         let newSquare = document.createElement("div");
         newSquare.setAttribute("class","grid-square");
         newSquare.setAttribute("style","border:0.5px solid black");
+        newSquare.setAttribute("toggled","false");
         grid.appendChild(newSquare);
     }
 
@@ -26,9 +26,19 @@ function createGrid(gridSize) {
         let target = event.target;
         let square = document.getElementById(`${target.id}`);
         square.style.backgroundColor = "black";
-        square.setAttribute("")
+        square.setAttribute("toggled","true");
     })
 
 }
+
+resetBtn.addEventListener('click', (event) => {
+    let grid = document.querySelector('.grid');
+    let gridChildren = grid.children;
+
+    for(let i = 0; i < gridChildren.length; i++){
+        gridChildren[i].style.backgroundColor = "white";
+    }
+
+})
 
 createGrid(256);
