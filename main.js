07bef2ef00a049c8248db68428911935cc144ac4
Ryan <ryankane180@gmail.com>
2023-10-31 11:06:31 +0000

    Resize functionality added

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
 // TO-DO:
 // 1. Rainbow module
 // 2. Pick Grid Size mode
-
+//let user enter number of grids w*l
+//error handle to ensure it is within range
 
 
 let resetBtn = document.querySelector('.reset');
 let rainbowBtn = document.querySelector('.rainbow');
+let resizeBtn = document.querySelector('.resize');
 let rainbowToggle = 0;
+let gridContainer = document.querySelector('.grid-container');
+const GRID_WIDTH = gridContainer.clientWidth;
+const DEFAULT_GRID_SIZE = 16;
+createGrid(DEFAULT_GRID_SIZE);
 
 
 function createGrid(gridSize) {
-    let grid = document.querySelector('.grid');
+    let grid = document.querySelector('.grid-container');
+    
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
 
-    for(let i = 0; i < gridSize; i++){
+    let totalSquares = gridSize * gridSize;
+    let squareWH = (GRID_WIDTH-gridSize) / gridSize;
+
+
+    for(let i = 0; i < totalSquares; i++){
         let newSquare = document.createElement("div");
         newSquare.setAttribute("class","grid-square");
-        newSquare.setAttribute("style","border:0.5px solid black");
+        newSquare.setAttribute("style","border:0.001px solid black");
         newSquare.setAttribute("toggled","false");
         grid.appendChild(newSquare);
     }
 
     let gridChildren = grid.children;
-
     for(let i = 0; i < gridChildren.length; i++){
-        gridChildren[i].style.width = "30px";
-        gridChildren[i].style.height = "30px";
+        gridChildren[i].style.width = `${squareWH}px`;
+        gridChildren[i].style.height = `${squareWH}px`;
+        gridChildren[i].style.backgroundColor = "beige";
         gridChildren[i].id = i;
     }
 
@@ -39,7 +53,6 @@ function createGrid(gridSize) {
                 square.style.backgroundColor = "black";
             }
         }
-        
         square.setAttribute("toggled","true");
     })
 
@@ -63,10 +76,16 @@ rainbowBtn.addEventListener('click', (event) => {
     }
 })
 
+resizeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let resizeInput = document.querySelector('.number-field');
+    let gridSize = Number(resizeInput.value);
+    createGrid(gridSize);
+})
+
 
 function getRandomColour() {
     let colourString = `${(Math.floor(Math.random() * (255 - 1 + 1) + 1))}, ${(Math.floor(Math.random() * (255 - 1 + 1) + 1))}, ${(Math.floor(Math.random() * (255 - 1 + 1) + 1))}`;
     return colourString; 
 }
 
-createGrid(256);
